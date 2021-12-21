1a106d4eaea43d2ff06b978b0d4321178f3a6efe
DataBeeGood <DataBeeGood@users.noreply.github.com>
2021-12-21 18:50:11 -0500

    Issues addressed
    Issues addressed

--- MMXXI/Assets/Scripts/MenuUIHandler.cs
+++ MMXXI/Assets/Scripts/MenuUIHandler.cs
@@ -2,14 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //BB for SceneManager.LoadScene method
+#if UNITY_EDITOR
+using UnityEditor;  //BB for Conditionals
+#endif
 
-// Sets the script to be executed later than all default scripts
-// This is helpful for UI, since other things may need to be initialized before setting the UI
+// Set this UI script to be executed later than all default scripts
+// Other scripts may need to be initialized before the UI is ready
+// Use DefaultExecutionOrder()
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
-    public ColorPicker ColorPicker;
-
+    //Customize the Color of an object
+	public ColorPicker ColorPicker;
+	
+	//Change the Color of an Object
     public void NewColorSelected(Color color)
     {
         // add code here to handle when a color is selected
@@ -17,8 +24,26 @@ public class MenuUIHandler : MonoBehaviour
     
     private void Start()
     {
-        ColorPicker.Init();
-        //this will call the NewColorSelected function when the color picker have a color button clicked.
+        //Initialize the ColorPicker
+		ColorPicker.Init();
+        
+		//Apply a Color change to the Color Picker
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    //BB Load a specified scene using built-in ordered list: Build List 
+    public void StartNewScene()
+    {
+        SceneManager.LoadScene(1); //BB (0) Scene is loaded at Start() 
+    }
+
+    //BB Exit the Application
+  public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
     }
 }
